b0e13e95f16d42dff4f54a3cb3667c59b3e4a0e7
koen <koenr14@live.nl>
2018-12-03 17:06:40 +0100

    took out react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(
-    <Router>
-        <App />
-    </Router>,
+    <App />,
     document.getElementById('root')
 );
 
